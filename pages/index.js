e62911bbfbc54b261cca78796973277ffe5d73cc
RutPatelSCIT <rutpatelscitpune@gmail.com>
2025-09-14 10:01:32 +0530

    Initial Next.js app

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Hello from Next.js + Netlify!</h1>
+      <p>This is my first Next.js app deployed to Netlify.</p>
+    </div>
+  );
+}
