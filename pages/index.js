02bace6da4f8206f0878f6af7a3bdfd21867cd77
RutPatelSCIT <92986759+RutPatelSCIT@users.noreply.github.com>
2025-09-14 11:30:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🚀 We are comming soon! </h1>
+      <Image
+        src="/unnamed.jpg"   // from public folder
+        alt="Apna Stocks Logo"
+        width={200}
+        height={200}
+      />
+      <h1>🚀 We are coming soon!</h1>
       <p>Please mail on rut@apnastocks.in</p>
     </div>
   );
